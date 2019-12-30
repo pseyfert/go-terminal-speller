e12539719b45057be2c87eba61234a52c350de67
Paul Seyfert <pseyfert.mathphys@gmail.com>
2019-12-30 01:36:01 +0100

    bodging hacking all bad code

--- pkg/rtoname.go
+++ pkg/rtoname.go
@@ -1,8 +1,6 @@
 package terminalspeller
 
 import (
-	_ "bufio"
-	_ "bytes"
 	// "golang.org/x/text/unicode/norm"
 	"golang.org/x/text/unicode/runenames"
 	"io"
@@ -11,12 +9,14 @@ import (
 )
 
 type translator struct {
-	w io.Writer
+	w            io.Writer
+	Didsomething bool
 }
 
-func NewTranslator(writer io.Writer) io.Writer {
+func NewTranslator(writer io.Writer) translator {
 	retval := translator{
-		w: writer,
+		w:            writer,
+		Didsomething: false,
 	}
 	return retval
 }
@@ -26,26 +26,27 @@ func (myself translator) Write(p []byte) (int, error) {
 	var retval int
 	for _, r := range p_string {
 		if unicode.IsSymbol(r) {
-			i, err := myself.w.Write([]byte{':'})
+			i, err := (myself.w).Write([]byte{':'})
 			if err != nil {
 				// FIXME: check 0
 				return 0, err
 			}
 			retval += i
-			i, err = myself.w.Write([]byte(strings.ReplaceAll(strings.ToLower(runenames.Name(r)), " ", "_")))
+			i, err = (myself.w).Write([]byte(strings.ReplaceAll(strings.ToLower(runenames.Name(r)), " ", "_")))
+			myself.Didsomething = true
 			if err != nil {
 				// FIXME: check 0
 				return 0, err
 			}
 			retval += i
-			i, err = myself.w.Write([]byte{':'})
+			i, err = (myself.w).Write([]byte{':'})
 			if err != nil {
 				// FIXME: check 0
 				return 0, err
 			}
 			retval += i
 		} else {
-			i, err := myself.w.Write([]byte(string(r)))
+			i, err := (myself.w).Write([]byte(string(r)))
 			if err != nil {
 				// FIXME: check 0
 				return 0, err
